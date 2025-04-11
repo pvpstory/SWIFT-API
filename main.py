91071803a34a632022f2f0701559918044539056
mikolaasinskij <koliyayyt@gmail.com>
2025-04-11 11:48:28 +0200

    test_insertion and insert_swift_data

--- main.py
+++ main.py
@@ -42,8 +42,6 @@ async def swift_codes(swift_code: str):
         swift_code = results[i][1]
         Name = results[i][2]
         address = results[i][3]
-        country_name = results[i][4]
-        is_headquarter = results[i][5]
         new_bank = {
             "address": address,
             "bankName": Name,
@@ -62,6 +60,64 @@ async def swift_codes(swift_code: str):
 
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/v1/swift-codes/country/{countryISO2code}")
+async def say_hello(contryISO2code: str):
+    print("131231231")
+    cur.execute("SELECT * from SWIFT_CODES WHERE COUNTRY_ISO2 = ?", (contryISO2code,))
+    results = cur.fetchall()
+    response = {
+        "countryISO2": contryISO2code,
+        "countryName": results[0][4],
+        "swiftCodes": []
+    }
+
+    for i,bank in enumerate(results):
+        countryISO2 = results[i][0]
+        swift_code = results[i][1]
+        Name = results[i][2]
+        address = results[i][3]
+
+        isHeadquarter = True
+        if results[i][5] == "0":
+            isHeadquarter = False
+
+        response["swiftCodes"].append({
+            "address": address,
+            "bankName": Name,
+            "countryISO2": countryISO2,
+            "isHeadquarter": isHeadquarter,
+            "swiftCode": swift_code
+
+        })
+    return response
+
+@app.post("/v1/swift-codes")
+async def insert_swift_data(swift_data: dict):
+    #make sure the data is legit later
+    #maby use fastapi models?
+    con = sqlite3.connect("SWIFT-CODES.db")
+    cur = con.cursor()
+
+    isheadquarter = False
+    if swift_data["swiftCode"][-3:] == "XXX":
+        isheadquarter = True
+    print(swift_data)
+    cur.execute(
+        "INSERT INTO SWIFT_CODES(COUNTRY_ISO2, SWIFT_CODE, NAME, ADDRESS, COUNTRY_NAME, ISHEADQUARTER) "
+        "VALUES (?, ?, ?, ?, ?, ?)",
+        (
+            swift_data["countryISO2"],
+            swift_data["swiftCode"],
+            swift_data["bankName"],
+            swift_data["address"],
+            swift_data["countryName"],
+            isheadquarter)
+    )
+
+    con.commit()
+
+    return {"message": "sukces"}
+
+
+
+
